faa178f5d32e49aabda83006168ceba2533974c0
Gabriel Cardoso <gabriel@upoutsourcing.com>
2024-07-24 19:29:45 -0300

    Refactor TodoList component to manage tasks and implement task actions

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,35 @@
 import styles from "./Card.module.css";
 import { Trash } from "@phosphor-icons/react";
-export function Card() {
+
+interface ICard {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+interface ICardProps {
+  task: ICard;
+  removeTask: (id: number) => void;
+  completeTask: (id: number) => void;
+}
+export function Card({ task, removeTask, completeTask }: ICardProps) {
+  const handleRemoveTask = (id: number) => {
+    removeTask(id);
+  };
   return (
     <div className={styles.wrapper}>
-      <input type="checkbox" className={styles.checkbox} />
-      <label className={styles.label}>Card</label>
-      <button className={styles.button}>
-        <Trash size={20}/>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={task.completed}
+        onChange={() => completeTask(task.id)}
+      />
+      <label className={styles.label}>{task.task}</label>
+      <button
+        type="button"
+        className={styles.button}
+        onClick={() => handleRemoveTask(task.id)}
+      >
+        <Trash size={20} />
       </button>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,18 +2,80 @@ import { CountHeader } from "./CountHeader";
 import styles from "./TodoList.module.css";
 import clipBoard from "../assets/clipboard.svg";
 import { Card } from "./Card";
+import { SubmitAction } from "./SubmitAction";
+import { useState } from "react";
+
+interface ITask {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 export function TodoList() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  const completedTasks = tasks.filter((task) => task.completed).length;
+
+  const addTask = (task: string) => {
+    const newTask = {
+      id: Math.random(),
+      task,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const removeTask = (id: number) => {
+    //reduce to remove value in array
+    const updatedTasks = tasks.reduce((acc, task) => {
+      if (task.id !== id) {
+        acc.push(task);
+      }
+      return acc;
+    }, [] as ITask[]);
+    setTasks(updatedTasks);
+  };
+
+  const completeTask = (id: number) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    // order finishied tasks to the end of the list
+    updatedTasks.sort((a, b) => {
+      if (a.completed && !b.completed) {
+        return 1;
+      }
+      if (!a.completed && b.completed) {
+        return -1;
+      }
+      return 0;
+    });
+    setTasks(updatedTasks);
+  };
+
   return (
     <>
-      <CountHeader />
+      <SubmitAction addTask={addTask} />
+      <CountHeader totalTasks={tasks.length} completedTasks={completedTasks} />
       <div className={styles.wrapper}>
-        <div className={styles.empty}>
-          <img src={clipBoard} alt="Clipboard" />
-          <h5>Você ainda não tem tarefas cadastradas</h5>
-          <label>Crie tarefas e organize seus itens a fazer</label>
-        </div>
-      <Card />
+        {tasks.length === 0 && (
+          <div className={styles.empty}>
+            <img src={clipBoard} alt="Clipboard" />
+            <h5>Você ainda não tem tarefas cadastradas</h5>
+            <label>Crie tarefas e organize seus itens a fazer</label>
+          </div>
+        )}
+        {tasks.map((task) => (
+          <Card
+            key={task.id}
+            task={task}
+            removeTask={removeTask}
+            completeTask={completeTask}
+          />
+        ))}
       </div>
     </>
   );
